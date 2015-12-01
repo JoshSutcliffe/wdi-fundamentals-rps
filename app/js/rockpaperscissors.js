82e5bea4580cb79f9d1fbec89d3df0d492ebed7b
Josh <joshclement12@gmail.com>
2015-12-01 21:58:52 +1100

    Unchecked homework from section 4

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -43,8 +43,30 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove === "rock" && computerMove === "scissors") {
+        result = "playerWins";
+}   else if (playerMove === "paper" && computerMove === "rock") {
+        result = "playerWins";
+}   else if (playerMove === "scissors" && computerMove === "paper") {
+        result = "playerWins";
+}   else if (computerMove === "rock" && playerMove === 'Scissors') {
+        result = "computerWins";
+}    else if (computerMove === "Paper" && playerMove === 'Rock') {
+        result = "computerWins";   
+}   else if (computerMove === "rock" && playerMove === 'Scissors') {
+        result = "computerWins";
+}    else if (computerMove === "rock" && playerMove === 'rock') {
+        result = "tie";
+}    else if (computerMove === "scissors" && playerMove === 'Scissors') {
+        result = "tie";
+}   else if (computerMove === "paper" && playerMove === 'paper') {    
+        result = "tie";
+}
     return winner;
 }
+/* above section is the homework from section 4. don't think this is the loop
+that the rest of the section was describing, so need to check that. Also, no idea
+what's going on with the return, so need to do that as well. */
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
